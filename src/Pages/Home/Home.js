d655a4e267c7199fef7998950aa3841f78b6756a
Danilo Mello <danilodemellow@gmail.com>
2020-08-23 00:31:11 -0300

    Fixing the hojtar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,13 +2,8 @@ import React from 'react';
 import { About } from '../../Components/About/About';
 import { Navigation } from '../../Components/Navigation/Navigation';
 import { HomePage } from './Style';
-import { hotjar } from 'react-hotjar';
 
 export const Home = () => {
-  React.useEffect(() => {
-    hotjar.initialize(956771, 6);
-  }, []);
-
   return (
     <HomePage>
       <About />
