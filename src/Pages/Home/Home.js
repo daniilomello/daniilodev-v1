db86610af7e534d9751c8892ca73122cb687275d
Danilo Mello <danilodemellow@gmail.com>
2020-08-22 23:59:00 -0300

    Fixing Tag problem

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,16 +3,10 @@ import { About } from '../../Components/About/About';
 import { Navigation } from '../../Components/Navigation/Navigation';
 import { HomePage } from './Style';
 import { hotjar } from 'react-hotjar';
-import { TagManager } from 'react-gtm-module';
-
-const tagManagerArgs = {
-  gtmId: 'GTM-W89NH7X',
-};
 
 export const Home = () => {
   React.useEffect(() => {
     hotjar.initialize(956771, 6);
-    TagManager.initialize(tagManagerArgs);
   }, []);
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-W89NH7X',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <React.StrictMode>
