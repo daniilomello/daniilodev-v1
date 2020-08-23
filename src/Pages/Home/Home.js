81f4fbec5c9bc08d02eae5d6337e35623249da23
Danilo Mello <danilodemellow@gmail.com>
2020-08-22 23:54:21 -0300

    Adding Tag Manager and Hotjar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { About } from '../../Components/About/About';
 import { Navigation } from '../../Components/Navigation/Navigation';
 import { HomePage } from './Style';
+import { hotjar } from 'react-hotjar';
+import { TagManager } from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-W89NH7X',
+};
 
 export const Home = () => {
+  React.useEffect(() => {
+    hotjar.initialize(956771, 6);
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <HomePage>
       <About />
