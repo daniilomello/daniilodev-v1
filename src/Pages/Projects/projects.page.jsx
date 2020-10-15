cbed1a665bdfdd27b8e37860a966d5509210025e
Danilo Mello <danilodemellow@gmail.com>
2020-10-15 06:35:56 -0300

    Adding new projects

--- src/Pages/Projects/projects.page.jsx
+++ src/Pages/Projects/projects.page.jsx
@@ -30,56 +30,70 @@ export const Projects = () => {
           <ProjectItem
             name="Site Pessoal"
             company="-"
-            tech="React · Styled-Components"
+            tech="React · Styled-Components · Netlify"
             linkgit="https://github.com/daniilomello"
             linksite="https://daniilo.dev"
           />
           <ProjectItem
+            name="Dogs Social"
+            company="-"
+            tech="React · Fetch API · Styled-Components · Wordpress API  · Netlify"
+            linkgit="https://github.com/daniilomello"
+            linksite="https://dogs.daniilo.dev/"
+          />
+          <ProjectItem
+            name="Smartbrain"
+            company="-"
+            tech="React · Express · Clarifai API · PostgreSQL · Heroku · Netlify"
+            linkgit="https://github.com/daniilomello"
+            linksite="https://smartbrain.daniilo.dev/"
+          />
+          <ProjectItem
             name="Website Agência R8"
             company="Agência R8"
-            tech="Front-end · Wordpress"
+            tech="PHP · JS · Wordpress"
             linksite="http://agenciar8.com.br/novo/"
           />
           <ProjectItem
             name="Website To be Comunição"
             company="To be Comunição"
-            tech="Front-end · Wordpress"
+            tech="PHP · JS · Wordpress"
             linksite="https://tobe.ppg.br/"
           />
           <ProjectItem
             name="APP Uberlândia"
             company="Agência R8"
-            tech="Front-end · Wordpress"
+            tech="PHP · JS · Wordpress"
             linksite="https://appuberlandia.com.br/"
           />
           <ProjectItem
             name="Educadora FM"
             company="To be Comunição"
-            tech="Front-end · Wordpress"
+            tech="PHP · JS · Wordpress"
             linksite="https://educadora909.com.br/"
           />
           <ProjectItem
             name="Studio Gláucia"
             company="Agência R8"
-            tech="Front-end · Woocommerce"
+            tech="PHP · JS · Wordpress · Woocommerce"
             linksite="https://studioglauciaxavier.com.br/"
           />
           <ProjectItem
             name="Paranaíba FM"
             company="To be Comunição"
-            tech="Front-end · Wordpress"
+            tech="PHP · JS · Wordpress"
             linksite="https://paranaibafm.com.br/"
           />
           <ProjectItem
             name="Joint Bee"
             company="Agência R8"
-            tech="Front-end · Wordpress"
+            tech="PHP · JS · Wordpress"
             linksite="https://www.jointbee.com/"
           />          
           <ProjectItem
             name="Palácio Motel"
             company="To be Comunição"
-            tech="Front-end · Wordpress"
+            tech="PHP · JS · Wordpress"
             linksite="http://palaciomotel.com.br/"
           />
         </tbody>
