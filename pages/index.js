39a3d85217a45f8f2875d611f8e738e5d53fdb16
Danilo Mello <danilodemellow@gmail.com>
2020-11-13 21:13:32 -0300

    installing NextJS

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Danilo Mello - Front-end Developer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Olá Mundo</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  )
+}
